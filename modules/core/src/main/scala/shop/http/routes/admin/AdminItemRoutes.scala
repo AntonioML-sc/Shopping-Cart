4dce1e3b2889f47b54f1f5219df51114030b88f2
Antonio Morales Ledesma <antoniomoralesledesma@gmail.com>
2021-05-24 11:39:31 +0200

    Add delete and modify to items

--- modules/core/src/main/scala/shop/http/routes/admin/AdminItemRoutes.scala
+++ modules/core/src/main/scala/shop/http/routes/admin/AdminItemRoutes.scala
@@ -6,15 +6,22 @@ import org.http4s.HttpRoutes
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
-import shop.domain.item.ItemParam
+import shop.domain.item.{ ItemId, ItemName, ItemParam }
 import shop.http.json.itemParamDecoder
 import shop.services.Items
+
+import java.util.UUID
 
 final class AdminItemRoutes[F[_]: Defer: Monad: JsonDecoder](items: Items[F])(implicit ME: MonadError[F, Throwable])
     extends Http4sDsl[F] {
   private[admin] val prefixPath: String = "/items"
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
-    case ar @ POST -> Root => ar.asJsonDecode[ItemParam].flatMap(ip => Created(items.create(ip.toDomain)))
+    case ar @ POST -> Root                 => ar.asJsonDecode[ItemParam].flatMap(ip => Created(items.create(ip.toDomain)))
+    case DELETE -> Root / "all"            => Ok(items.clearAll)
+    case DELETE -> Root / "byId" / id      => Ok(items.deleteById(ItemId(UUID.fromString(id))))
+    case DELETE -> Root / "byName" / iName => Ok(items.deleteByName(ItemName(iName)))
+    case ar @ PUT -> Root / oldName =>
+      ar.asJsonDecode[ItemParam].flatMap(ip => Ok(items.modifyByName(ItemName(oldName), ip.toDomain)))
   }
   val routes: HttpRoutes[F] = Router(prefixPath -> httpRoutes)
 }
--- modules/core/src/main/scala/shop/services/items.scala
+++ modules/core/src/main/scala/shop/services/items.scala
@@ -3,7 +3,7 @@ package shop.services
 import cats.effect.{ Bracket, BracketThrow, Resource }
 import cats.implicits._
 import shop.domain.item.{ Item, ItemId, ItemName }
-import shop.services.ItemSQL.{ insertItem, selectAll }
+import shop.services.ItemSQL.{ deleteAll, deleteItemById, deleteItemByName, insertItem, modifyItemByName, selectAll }
 import skunk._
 import skunk.codec.all._
 import skunk.{ Codec, Command, Query }
@@ -14,6 +14,10 @@ import java.util.UUID
 trait Items[F[_]] {
   def findAll: F[List[Item]]
   def create(name: ItemName): F[Unit]
+  def deleteById(id: ItemId): F[Unit]
+  def deleteByName(name: ItemName): F[Unit]
+  def clearAll: F[Unit]
+  def modifyByName(oldName: ItemName, newName: ItemName): F[Unit]
 }
 
 object Items {
@@ -28,6 +32,17 @@ final class LiveItems[F[_]: BracketThrow](sessionPool: Resource[F, Session[F]]) 
     sessionPool.use { session =>
       session.prepare(insertItem).use(cmd => cmd.execute(name.toItem(ItemId(UUID.randomUUID()))).void)
     }
+
+  override def deleteById(id: ItemId): F[Unit] =
+    sessionPool.use(session => session.prepare(deleteItemById).use(cmd => cmd.execute(id).void))
+
+  override def deleteByName(name: ItemName): F[Unit] =
+    sessionPool.use(session => session.prepare(deleteItemByName).use(cmd => cmd.execute(name).void))
+
+  override def clearAll: F[Unit] = sessionPool.use(session => session.execute(deleteAll).void)
+
+  override def modifyByName(oldName: ItemName, newName: ItemName): F[Unit] =
+    sessionPool.use(session => session.prepare(modifyItemByName).use(cmd => cmd.execute(oldName ~ newName).void))
 }
 
 private object ItemSQL {
@@ -45,5 +60,25 @@ private object ItemSQL {
     sql"""
          INSERT INTO items
          VALUES ($item)
+       """.command
+  val deleteItemById: Command[ItemId] =
+    sql"""
+         DELETE FROM items
+         WHERE uuid = $itemId
+       """.command
+  val deleteItemByName: Command[ItemName] =
+    sql"""
+         DELETE FROM items
+         WHERE name = $itemName
+       """.command
+  val deleteAll: Command[Void] =
+    sql"""
+         DELETE FROM items
+       """.command
+  val modifyItemByName: Command[ItemName ~ ItemName] =
+    sql"""
+         UPDATE items
+         SET name = $itemName
+         WHERE name = $itemName
        """.command
 }
